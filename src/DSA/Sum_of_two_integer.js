da6598848d0dfbfaf0693fef6f64a15f19d7f370
Akhilesh verma <Akhileshverma207@gmail.com>
2025-11-09 23:44:14 +0530

    add sum of two integer

---
+++ src/DSA/Sum_of_two_integer.js
@@ -0,0 +1,7 @@
+
+const Sum_of_two_integer = () => {return (<div></div>)};export default Sum_of_two_integer
+
+let a = 10;
+let b = 20;
+
+console.log("sum of 10 and 20 is" + a + b)  // "sum of 10 and 20 is" 1020
